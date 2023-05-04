2ea23c37ae542f74d3577f720d8a2031e6025ec6
dzellmer <dzellmer@andrew.cmu.edu>
2023-05-03 23:24:55 -0400

    Update logic in recommendation handler

--- server/src/controllers/recommendation.controller.ts
+++ server/src/controllers/recommendation.controller.ts
@@ -11,68 +11,82 @@ const router = express.Router();
  **************************** */
 
 // Get recommendations
-router.get('/:userId', async (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
     try {
         // extract userId
-        const userId = req.params.userId;
+        let userId;
+        if (req.query.userId) { userId = req.query.userId }
 
         let boundaryId;
-        if (req.query.boundaryId) { boundaryId = req.query.boundaryId; console.log('Boundary Id', boundaryId) }
+        if (req.query.boundaryId) { boundaryId = req.query.boundaryId }
 
         // get swipes by userId
-        const userSwipes = await SwipeModel.findOne({ userId: userId }).exec();
-        
-        // if swipes.likes is not empty
-        if (userSwipes) {
-            // get all recommendations by liked clothingId
-            let recommendations: string[] = [];
-            for (const aId of userSwipes.likes) {
-                const rDoc = await RecommendationModel.findOne({ id: aId }).exec();
-                if (rDoc) {
-                    recommendations = [ ...recommendations, ...rDoc.recommendations];
+        if (userId) {
+            const userSwipes = await SwipeModel.findOne({ userId: userId }).exec();
+            
+            // if swipes.likes is not empty
+            if (userSwipes) {
+                // get all recommendations by liked clothingId
+                let recommendations: string[] = [];
+                for (const aId of userSwipes.likes) {
+                    const rDoc = await RecommendationModel.findOne({ id: aId }).exec();
+                    if (rDoc) {
+                        recommendations = [ ...recommendations, ...rDoc.recommendations];
+                    }
                 }
-            }
-            // deduplicate recommendations
-            recommendations = Array.from(new Set(recommendations));
-            // get first 10 articles by articleId
-            let recommendedClothing;
-
-            if (boundaryId) {
-                recommendedClothing = await ClothingModel.find({ _id: { $lt: boundaryId }, id: { $in: recommendations }}).sort({ _id: -1 }).limit(10);
-            } else {
-                recommendedClothing = await ClothingModel.find({ id: { $in: recommendations }}).limit(10);
-            }
-            // return
-            res.json({ recommendations: recommendedClothing });
-        } else {
-            let recommendedClothing;
-
-            const sr = await SurveyModel.findOne({ userId: userId }).exec();
-            if (sr) {
-                const query: {[k: string]: any} = {};
-
-                const preferredStyles = sr.response.style;
-                const preferredPatterns = sr.response.pattern;
-                const preferredColors = sr.response.color;
-                const preferredPriceRange = sr.response.price;
-
-                if (preferredStyles) { query.usage = { $in: preferredStyles } }
-                if (preferredPatterns) { query.pattern = { $in: preferredPatterns } }
-                if (preferredColors) { query.color = { $in: preferredColors } }
-                if (preferredPriceRange) { query.price = { $gte: preferredPriceRange[0], $lte: preferredPriceRange[1] } }
-
-                if (boundaryId) { query._id = { $lt: boundaryId } }
-
-                recommendedClothing = await ClothingModel.find(query).limit(10);
-            } else {
-                console.log(boundaryId)
+                // deduplicate recommendations
+                recommendations = Array.from(new Set(recommendations));
+                // get first 10 articles by articleId
+                let recommendedClothing;
+    
                 if (boundaryId) {
-                    recommendedClothing = await ClothingModel.find({ _id: { $lt: boundaryId } }).limit(10);
+                    recommendedClothing = await ClothingModel.find({ _id: { $lt: boundaryId }, id: { $in: recommendations }}).sort({ _id: -1 }).limit(10);
                 } else {
-                    recommendedClothing = await ClothingModel.find().limit(10);
+                    recommendedClothing = await ClothingModel.find({ id: { $in: recommendations }}).limit(10);
+                }
+                // return
+                res.json({ 
+                    boundaryId: recommendedClothing.length > 0 ? recommendedClothing.slice(-1)[0]._id : null,
+                    recommendations: recommendedClothing 
+                });
+            } else {
+                let recommendedClothing;
+    
+                const sr = await SurveyModel.findOne({ userId: userId }).exec();
+                if (sr) {
+                    const query: {[k: string]: any} = {};
+    
+                    const preferredStyles = sr.response.style;
+                    const preferredPatterns = sr.response.pattern;
+                    const preferredColors = sr.response.color;
+                    const preferredPriceRange = sr.response.price;
+    
+                    if (preferredStyles) { query.usage = { $in: preferredStyles } }
+                    if (preferredPatterns) { query.pattern = { $in: preferredPatterns } }
+                    if (preferredColors) { query.color = { $in: preferredColors } }
+                    if (preferredPriceRange) { query.price = { $gte: preferredPriceRange[0], $lte: preferredPriceRange[1] } }
+    
+                    if (boundaryId) { query._id = { $lt: boundaryId } }
+    
+                    recommendedClothing = await ClothingModel.find(query).limit(10);
+                    res.json({ 
+                        boundaryId: recommendedClothing.length > 0 ? recommendedClothing.slice(-1)[0]._id : null,
+                        recommendations: recommendedClothing 
+                    });
+                } else {
+                    if (boundaryId) {
+                        recommendedClothing = await ClothingModel.find({ _id: { $lt: boundaryId } }).limit(10);
+                    } else {
+                        recommendedClothing = await ClothingModel.find().limit(10);
+                    }
+                    res.json({ 
+                        boundaryId: recommendedClothing.length > 0 ? recommendedClothing.slice(-1)[0]._id : null,
+                        recommendations: recommendedClothing 
+                    });
                 }
             }
-            res.json({ recommendations: recommendedClothing })
+        } else {
+            res.sendStatus(404);
         }
     } catch (error) {
         if (error instanceof Error) {
